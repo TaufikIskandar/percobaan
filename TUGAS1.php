3e4758b5e464c89f6a4bc039c4c33fc940566989
andi05 <andi.doank91@yahoo.com>
2014-05-07 17:23:37 +0700

    Update TUGAS1.php
    117006312,117006338

--- TUGAS1.php
+++ TUGAS1.php
@@ -7,3 +7,25 @@ $xpath = new DomXPath($dom);
 $judul = $xpath->query("/html/body/div/div/div[5]/div/div[2]/div/div[5]/p[2]");
 foreach ($judul as $jdl) {
     echo $jdl->nodeValue;
+
+};
+echo "<br/><br/>";
+$dom = new DomDocument;
+$dom->loadHTMLFile("http://www.tempo.co/read/news/2014/05/06/064575884/Omset-Bakso-Babi-Sutiman-Rp-30-Juta-per-Bulan");
+$xpath = new DomXPath($dom);
+$judul = $xpath->query("/html/body/div/div/div[5]/div/div[2]/div/h1");
+foreach ($judul as $jdl) {
+    echo $jdl->nodeValue;
+
+};
+echo "<br/>";
+$dom = new DomDocument;
+$dom->loadHTMLFile("http://www.tempo.co/read/news/2014/05/06/064575884/Omset-Bakso-Babi-Sutiman-Rp-30-Juta-per-Bulan");
+$xpath = new DomXPath($dom);
+$judul = $xpath->query("/html/body/div/div/div[5]/div/div[2]/div/div[5]/p[2]");
+foreach ($judul as $jdl) {
+    echo $jdl->nodeValue;
+
+}
+
+?>
